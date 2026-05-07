8dca1fe642d3cca5e08d116b7aa974dabc809a0d
RaSyl31 <sylviorakotoarivelo@gmail.com>
2026-05-07 14:37:00 +0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -617,7 +617,7 @@ components.html(f"""
     </span>
 
     <img src="data:image/png;base64,{logo_edf}"
-         width="95"
+         width="125"
          style="vertical-align:middle; margin-left:16px;">
 </div>
 """, height=95)
